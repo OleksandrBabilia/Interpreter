8617d2ffa5e628e6999808dc249667090d4aa296
AlexBabilya <djesssash1708@gmail.com>
2024-05-11 14:44:31 +0300

    Added: Main

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"interpreter/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is Interpreter Shell by Oleksandr Babilia!\n", user.Username)
+	fmt.Printf("Feel Fee to Type in Command\n")
+	repl.Strat(os.Stdin, os.Stdout)
+}
