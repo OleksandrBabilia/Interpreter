48afd8b23db535ac4b2ecba12340114af50567c9
AlexBabilya <djesssash1708@gmail.com>
2024-05-17 20:56:07 +0300

    Added: IntegerLiteral Parse

--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,8 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
+
 	"interpreter/ast"
 	"interpreter/lexer"
 	"interpreter/token"
@@ -42,6 +44,8 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.pareseIntegerLiteral)
+
 	return p
 }
 
@@ -150,6 +154,21 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 	leftExp := prefix()
 	return leftExp
+}
+
+func (p *Parser) pareseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+
+	return lit
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
